3b6302674740606ee89e083f7626438ffb1c7d9a
Diego García Santos <146078485+XxDonPolloxX@users.noreply.github.com>
2023-12-24 11:09:49 +0100

    gestor bungalos (falta que solo se pueda eliminar uno sin reservas)

--- src/GestorBungalos.java
+++ src/GestorBungalos.java
@@ -11,28 +11,83 @@ public class GestorBungalos {
     public ArrayList<Bungalo> getBungalos(){
         return bungalos;
     }
-    public void agregar_bungalo(Bungalo b){
+    public void agregar_bungalo(){
+        String opcion = "n", nombre;
+        int capacidad;
+        float precio;
+        Bungalo b;
+        System.out.println("Desea añadir un bungalo adaptado s/n:");
+        opcion = MyInput.readString();
+        System.out.println("Introduzca el nombre:");
+        nombre = MyInput.readString();
+        System.out.println("Introduzca la capacidad:");
+        capacidad = MyInput.readInt();
+        System.out.println("Introduzca el precio:");
+        precio = MyInput.readFloat();
 
+        if(opcion.equals("s")){
+            b = new BungaloAdaptado(nombre, capacidad, precio);
+        }
+        else{
+            b = new Bungalo(nombre, capacidad, precio);
+        }
         bungalos.add(b);
         System.out.println("Bungalo " + b.getId() +  " añadido con éxito!");
     }
-    public void eliminar_bungalo(Bungalo b){
-        bungalos.remove(b);
-        System.out.println("Bungalo " + b.getId() +  " eliminado con éxito!");
+    public void eliminar_bungalo(){ //Aqui falta comprobar si el bungalo tiene reservas
+        String id;
+        int i;
+        System.out.print("Introduce el id del bungalo a eliminar:");
+        id = MyInput.readString();
+        for(i=0;i<bungalos.size();i++){
+            if(id.equals(bungalos.get(i).getId())){
+                System.out.println("Bungalo " + bungalos.get(i).getId() +  " eliminado con éxito!");
+                bungalos.remove(bungalos.get(i));
+
+            }
+        }
+
     }
     public Bungalo recuperar_bungalo(String id){
-
         int i;
         for(i=0;i<bungalos.size();i++)
         {
             if(bungalos.get(i).getId().equals(id)) {
-                System.out.println(bungalos.get(i).getNombre());
+                System.out.println("Nombre: " + bungalos.get(i).getNombre());
+                System.out.println("Precio: " + bungalos.get(i).getPrecio());
+                System.out.println("Capacidad: " + bungalos.get(i).getCapacidad());
                 return bungalos.get(i);
                 }
             }
         System.out.println("Bungalo no encontrado");
         return null;
         }
+        public String mostrarBungalos(){
+        int i;
+        String opcion;
+        System.out.println("¿Desea mostrar la información de bungalos adaptados? En caso negativo se mostrará la información de los no adaptados (s/n):");
+        opcion = MyInput.readString();
+        String cadena_bungalos= "";
+            if(opcion.equals("s")){
+                for(i=0;i<bungalos.size();i++){
+                    if("BA".equals(bungalos.get(i).getId().substring(0, 2))){
+                        cadena_bungalos += bungalos.get(i).getId() + ": " + bungalos.get(i).getCapacidad() + " ";
+
+                    }
+                }
+
+            }
+            else{
+                for(i=0;i<bungalos.size();i++){
+                    if(!"BA".equals(bungalos.get(i).getId().substring(0,2))){
+                        cadena_bungalos += bungalos.get(i).getId() + ": " + bungalos.get(i).getCapacidad() + " ";
+                    }
+                }
+            }
+        return cadena_bungalos;
+        }
+
+
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        Actividad a = new Actividad("fsfsgsf", 34);
-        Restaurante r = new Restaurante("rrrr", 324);
-        System.out.println(a.getId());
-        System.out.println(r.getId());
-        Bungalo b = new BungaloAdaptado("Paco bungalos", 1, 1);
-        System.out.println(b.getNombre());
-        ServicioCatering prueba = new ServicioCatering(new ServicioAsistente(b));
-        System.out.println(prueba.getNombre());
-        System.out.println(b.getId());
-        System.out.println(prueba.getId());
 
+        GestorBungalos g = new GestorBungalos();
+        g.agregar_bungalo();
+        g.agregar_bungalo();
+        g.recuperar_bungalo("BA0");
     }
 }
